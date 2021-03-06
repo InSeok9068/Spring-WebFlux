e4841d6e718449ff210eccb40ca3428b3f35638a
Lee In Seok <dlstjr9068@gmail.com>
2021-03-06 18:40:55 +0900

    Initial commit

---
+++ src/test/java/kr/co/study/spring/webflux/SpringWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.study.spring.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
